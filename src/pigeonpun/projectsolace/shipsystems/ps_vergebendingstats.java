317a7fddfe2321ce2689ddf5a4725f7b8c9a3960
CosmicFishi <lyquocdung099@gmail.com>
2023-09-30 22:43:44 +0700

    more stuffs, finishing Sarpdeon

---
+++ src/pigeonpun/projectsolace/shipsystems/ps_vergebendingstats.java
@@ -0,0 +1,99 @@
+package pigeonpun.projectsolace.shipsystems;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.WeaponAPI;
+import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
+import com.fs.starfarer.api.util.IntervalUtil;
+import com.fs.starfarer.api.util.Misc;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class ps_vergebendingstats extends BaseShipSystemScript {
+    private static final float WEAPON_DAMAGE_BONUS_MULT = 2f;
+    private static final float FLUX_REDUCTION_PERCENTAGE = 20f;
+    private static final String BUILT_IN_W_ID = "ps_roseae";
+    private static final String BUILT_IN_SMOKE_ID = "ps_smoke_launcher";
+    private boolean fireOnce = false;
+
+    public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
+        ShipAPI ship = (ShipAPI) stats.getEntity();
+        CombatEngineAPI engine = Global.getCombatEngine();
+
+        if(ship != null) {
+            //active -> buff -> fire
+            if(state.equals(State.ACTIVE)) {
+                if(ship.getShield() != null) {
+                    ship.getShield().toggleOff();
+                }
+                stats.getEnergyWeaponFluxCostMod().modifyMult(id, (100f - FLUX_REDUCTION_PERCENTAGE) /100);
+                stats.getEnergyWeaponDamageMult().modifyMult(id, WEAPON_DAMAGE_BONUS_MULT);
+                stats.getBallisticWeaponDamageMult().modifyMult(id, WEAPON_DAMAGE_BONUS_MULT);
+
+                for (WeaponAPI w : ship.getAllWeapons()) {
+                    if (w.getSlot().isSystemSlot()) continue;
+                    if(state.equals(State.OUT)) {
+                        if (w.getSlot().isDecorative() && w.getId().equals(BUILT_IN_SMOKE_ID)) {
+                            w.setForceFireOneFrame(true);
+                        }
+                    }
+                    if (state.equals(State.ACTIVE)) {
+                        //refire built in
+                        if(!fireOnce) {
+                            w.setRefireDelay(w.getSpec().getChargeTime());
+                            if (Objects.equals(w.getId(), BUILT_IN_W_ID)) {
+                                w.setForceFireOneFrame(true);
+                                fireOnce = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            //jitter
+            float jitterLevel = effectLevel;
+            float jitterRangeBonus = 0;
+            float maxRangeBonus = 10f;
+            if (state == State.IN) {
+                jitterLevel = effectLevel / (1f / ship.getSystem().getChargeUpDur());
+                if (jitterLevel > 1) {
+                    jitterLevel = 1f;
+                }
+                jitterRangeBonus = jitterLevel * maxRangeBonus;
+            } else if (state == State.ACTIVE) {
+                jitterLevel = 1f;
+                jitterRangeBonus = maxRangeBonus;
+            } else if (state == State.OUT) {
+                jitterRangeBonus = jitterLevel * maxRangeBonus;
+            }
+            jitterLevel = (float) Math.sqrt(jitterLevel);
+            effectLevel *= effectLevel;
+
+            ship.setJitterUnder(this, new Color(52, 226, 79, 255), jitterLevel, 5, 0f, 7f + jitterRangeBonus);
+            //unmodify when out of active state
+            if(state.equals(State.OUT)) {
+                stats.getEnergyWeaponFluxCostMod().unmodify(id);
+                stats.getEnergyWeaponDamageMult().modifyMult(id, WEAPON_DAMAGE_BONUS_MULT);
+                stats.getBallisticWeaponDamageMult().modifyMult(id, WEAPON_DAMAGE_BONUS_MULT);
+            }
+            if(state.equals(State.COOLDOWN) || state.equals(State.IDLE)) {
+                fireOnce = false;
+            }
+        }
+    }
+    //dont apply cuz runScriptWhileIdle is true
+    public void unapply(MutableShipStatsAPI stats, String id) {}
+
+    public StatusData getStatusData(int index, State state, float effectLevel) {
+        if (index == 0) {
+            return new StatusData("Overclocking weapon", false);
+        }
+        if (index == 1) {
+            return new StatusData("Increasing time dilation", false);
+        }
+        return null;
+    }
+}
--- src/pigeonpun/projectsolace/weapons/ps_roseaeeffects.java
+++ src/pigeonpun/projectsolace/weapons/ps_roseaeeffects.java
@@ -2,6 +2,7 @@ package pigeonpun.projectsolace.weapons;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
+import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI;
 import com.fs.starfarer.api.util.IntervalUtil;
 import com.fs.starfarer.api.util.WeightedRandomPicker;
 import org.apache.log4j.Logger;
@@ -11,9 +12,11 @@ import pigeonpun.projectsolace.com.ps_vsguidedprojectilescript;
 import java.awt.*;
 import java.util.Objects;
 
-public class ps_roseaeeffects implements EveryFrameWeaponEffectPlugin, OnFireEffectPlugin {
+public class ps_roseaeeffects implements EveryFrameWeaponEffectPlugin, OnFireEffectPlugin, OnHitEffectPlugin {
     public static final Logger log = Global.getLogger(ps_roseaeeffects.class);
     private static final String ROSEAE_ID = "ps_roseae";
+    private static final float EMP_COUNT = 2f;
+    private static final float FIXED_SLOW = 0.15f;
     @Override
     public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
         ShipAPI ship = weapon.getShip();
@@ -23,5 +26,33 @@ public class ps_roseaeeffects implements EveryFrameWeaponEffectPlugin, OnFireEff
     public void onFire(DamagingProjectileAPI projectile, WeaponAPI weapon, CombatEngineAPI engine) {
         ShipAPI ship = weapon.getShip();
         engine.addPlugin(new ps_vsguidedprojectilescript(projectile, ship.getShipTarget()));
+    }
+
+    @Override
+    public void onHit(DamagingProjectileAPI projectile, CombatEntityAPI target, Vector2f point, boolean shieldHit, ApplyDamageResultAPI damageResult, CombatEngineAPI engine) {
+        float emp = projectile.getEmpAmount();
+        float dam = 0;
+        float count = 0;
+        if(shieldHit) {
+            while(count < EMP_COUNT) {
+                count++;
+                engine.spawnEmpArcPierceShields(
+                        projectile.getSource(), point, target, target,
+                        DamageType.ENERGY,
+                        dam, // damage
+                        emp, // emp
+                        100000f, // max range
+                        "tachyon_lance_emp_impact",
+                        20f, // thickness
+                        //new Color(25,100,155,255),
+                        //new Color(255,255,255,255)
+                        new Color(125,125,100,255),
+                        new Color(255,255,255,255)
+                );
+            }
+        }
+        float mult=1;
+        if(shieldHit)mult=0.5f;
+        target.getVelocity().scale(FIXED_SLOW*mult);
     }
 }
