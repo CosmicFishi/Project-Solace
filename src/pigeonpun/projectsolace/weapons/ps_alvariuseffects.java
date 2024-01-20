a7988e712e6260972802effad89c91b90a0c4ef6
CosmicFishi <lyquocdung099@gmail.com>
2024-01-20 08:46:39 +0700

    Forgot to add the boundless contaimination to Alvarius

--- src/pigeonpun/projectsolace/weapons/ps_alvariuseffects.java
+++ src/pigeonpun/projectsolace/weapons/ps_alvariuseffects.java
@@ -1,13 +1,23 @@
 package pigeonpun.projectsolace.weapons;
 
 import com.fs.starfarer.api.combat.*;
+import pigeonpun.projectsolace.com.ps_boundlesseffect;
 
 public class ps_alvariuseffects implements EveryFrameWeaponEffectPlugin, OnFireEffectPlugin {
+    private boolean runOnce = false;
     public static final float FLARE_SPAWN_COUNTER = 50f; //shots take to spawn flare
     public float shotCounter = 0;
 
     @Override
     public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
+        ShipAPI ship = weapon.getShip();
+        if (engine.isPaused()) return;
+        if(!runOnce){
+            if(!weapon.getSlot().isBuiltIn()){
+                ps_boundlesseffect.checkIfNeedAdding(ship.getVariant());
+            }
+            runOnce=true;
+        }
         if(this.shotCounter > FLARE_SPAWN_COUNTER) {
             this.shotCounter =0;
             //do the funny
